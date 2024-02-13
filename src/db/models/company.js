fd8cc19e009aea1512e069169f60200cf4f722ce
arsh-dhaliwal <155694537+arsh-dhaliwal@users.noreply.github.com>
2024-02-13 17:12:27 -0500

    Update company.js

--- src/db/models/company.js
+++ src/db/models/company.js
@@ -1,7 +1,6 @@
-const Database = require('better-sqlite3');
 const db = require('../initDb.js');
 
-const Company = db.prepare(`CREATE TABLE IF NOT EXISTS Companies (
+db.prepare(`CREATE TABLE IF NOT EXISTS Company (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   companyName TEXT NOT NULL,
   address TEXT NOT NULL,
@@ -13,46 +12,6 @@ const Company = db.prepare(`CREATE TABLE IF NOT EXISTS Companies (
   email TEXT NOT NULL,
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
-)`).run()('Company', {
-  companyName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  address: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  city: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  state: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  country: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  zipCode: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phoneNumber: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
-  }
-}, {
-  // Additional model options go here
-  timestamps: false
-});
-
+)`).run();
 // Export the model so it can be used in other parts of the application
 module.exports = Company;
